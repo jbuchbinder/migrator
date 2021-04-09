a682d3efe1f3b7d6a67237aa975c67d62f33ee6f
Jeff Buchbinder <jeff@ourexchange.net>
2021-04-09 12:00:33 -0400

    Transformer[js]: Initial commit, work in progress

--- go.mod
+++ go.mod
@@ -6,9 +6,11 @@ require (
 	github.com/beeker1121/goque v2.0.1+incompatible
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/robertkrimen/otto v0.0.0-20200922221731-ef014fd054ac
 	github.com/sirupsen/logrus v1.7.0
 	github.com/syndtr/goleveldb v1.0.0 // indirect
 	go.elastic.co/apm/module/apmsql v1.4.0
 	google.golang.org/appengine v1.6.0 // indirect
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
---
+++ transformer_js.go
@@ -0,0 +1,78 @@
+package migrator
+
+import (
+	"github.com/robertkrimen/otto"
+	_ "github.com/robertkrimen/otto/underscore"
+)
+
+var (
+	ottoInitialized bool
+	ottoObj         *otto.Otto
+)
+
+func init() {
+	TransformerMap["js"] = TableRenamerJavascript
+}
+
+// TableRenamerTransformer adjusts the table name of a destination table based
+// on the "TableName" parameter passed.
+var TableRenamerJavascript = xxx func(dbName, tableName string, data []SQLRow, params *Parameters) []TableData {
+	debug := paramBool(*params, "Debug", false)
+
+	method, ok := (*params)["METHOD"].(string)
+	if !ok {
+		method = ""
+	}
+
+	start := time.Now()
+	defer func() {
+		duration := time.Since(start)
+		if caught := recover(); caught != nil {
+			if caught == errHalt {
+				logger.Warnf("transformer[js]: Timed out after %d sec", timeout)
+				return
+			}
+			panic(caught)
+		}
+		logger.Debugf("transformer[js]: Completed execution in %v", duration)
+	}()
+
+	obj.vm.Interrupt = make(chan func(), 1)
+	go func(timeout int) {
+		time.Sleep(time.Duration(timeout) * time.Second)
+		ottoObj.Interrupt <- func() {
+			panic(errHalt)
+		}
+	}(timeout)
+
+	log.Printf("transformer[js]: Beginning execution")
+	_, err := ottoObj.Run(code)
+	if err != nil {
+		log.Warnf("transformer[js]: Returned %#v", err)
+	}
+
+	return []TableData{
+		{
+			DbName:    dbName,
+			TableName: tableName,
+			Data:      data,
+			Method:    method,
+		},
+	}
+}
+
+func initializeJsEnvironment() error {
+	if ottoInitialized {
+		return nil
+	}
+	ottoObj = otto.New()
+
+	ottoObj.vm.Set("log", func(call otto.FunctionCall) otto.Value {
+		passedVal, _ := call.Argument(0).ToString()
+		logger.Debugf("transformer[js]: %s: %s", obj.user.Name, passedVal)
+		return otto.Value{}
+	})
+
+	ottoInitialized = true
+	return nil
+}
